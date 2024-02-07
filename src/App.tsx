324653b08ae10cbca79f2102dd2a68863e568d28
sueka <mail@sueka.me>
2024-02-08 05:33:11 +0900

    :recycle: Fix the component layout less early
    Make sure the snapshot of <App> has not changed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Container } from '@mantine/core'
+import { Container, Grid } from '@mantine/core'
 
 import VCardForm from './VCardForm'
 
 const App = () => (
   <Container>
-    <VCardForm />
+    <Grid>
+      <Grid.Col span={{ xs: 12, sm: 6, md: 4 }}>
+        <VCardForm />
+      </Grid.Col>
+    </Grid>
   </Container>
 )
 
--- src/VCardForm.tsx
+++ src/VCardForm.tsx
@@ -1,15 +1,11 @@
-import { Grid } from '@mantine/core'
-
 import FnInput from './FnInput'
 import NInput from './NInput'
 
 const VCardForm: React.FC = () => (
-  <Grid>
-    <Grid.Col span={{ xs: 12, sm: 6, md: 4 }}>
-      <FnInput />
-      <NInput />
-    </Grid.Col>
-  </Grid>
+  <>
+    <FnInput />
+    <NInput />
+  </>
 )
 
 export default VCardForm
