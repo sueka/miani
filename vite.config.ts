36485c87ae84aebe9e5d1e3bd12e0910cd6e9ea7
sueka <mail@sueka.me>
2024-02-16 04:42:37 +0900

    :sparkles: :wrench: Setup Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import preact from '@preact/preset-vite'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'miani',
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
