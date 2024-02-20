69a9a1808ffca44d4c84ca26f61116ccd052584a
sueka <mail@sueka.me>
2024-02-21 06:31:24 +0900

    :recycle: Wrap it with <React.StrictMode>

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.layer.css'
 import { DatesProvider } from '@mantine/dates'
 import '@mantine/dates/styles.layer.css'
-import { render } from 'react'
+import React, { render } from 'react'
 import { RecoilRoot } from 'recoil'
 import 'temporal-polyfill/global'
 
@@ -11,18 +11,20 @@ import './lib/Iso8601DateTime/String.prototype.toDate'
 import exit from './lib/exit'
 
 render(
-  <MantineProvider defaultColorScheme="auto">
-    <RecoilRoot>
-      <DatesProvider
-        settings={{
-          firstDayOfWeek: 0,
-          weekendDays: [0],
-          timezone: 'UTC',
-        }}
-      >
-        <App />
-      </DatesProvider>
-    </RecoilRoot>
-  </MantineProvider>,
+  <React.StrictMode>
+    <MantineProvider defaultColorScheme="auto">
+      <RecoilRoot>
+        <DatesProvider
+          settings={{
+            firstDayOfWeek: 0,
+            weekendDays: [0],
+            timezone: 'UTC',
+          }}
+        >
+          <App />
+        </DatesProvider>
+      </RecoilRoot>
+    </MantineProvider>
+  </React.StrictMode>,
   document.getElementById('app') ?? exit(),
 )
