cb07da38d9c519553246b2864da0240ce7383161
sueka <mail@sueka.me>
2024-02-26 08:19:44 +0900

    :boom: Change the function specifications

--- src/lib/Iso8601DateTime/isIso8601DateTime.ts
+++ src/lib/Iso8601DateTime/isIso8601DateTime.ts
@@ -11,8 +11,11 @@ const offset = r`${hour}(?::?${minute})?`
 const date = r`${year}-${month}-${day}`
 const time = r`T${hour}:${minute}(?::${second}(?:.${milliseconds})?)?` // If you omit seconds, you must also omit milliseconds.
 const timeZone = r`Z|[+-]${offset}`
-const dateTime = r`${date}${time}?${timeZone}?`
+const dateTime = r`${date}${time}${timeZone}`
 
+/**
+ * Tests if `text` is an ISO 8601 full date and time.
+ */
 export default function isIso8601DateTime(
   text: string,
 ): text is Iso8601DateTime {
