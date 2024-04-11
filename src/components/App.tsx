11f7253a63a295aeec838b639643b86e55022aec
sueka <mail@sueka.me>
2024-04-11 19:22:48 +0900

    :sparkles: Display an app version number

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { Container, Group, Stack } from '@mantine/core'
+import { Container, Group, Stack, Text, Title } from '@mantine/core'
 import cls from 'classnames'
 
 import classes from './App.module.css'
@@ -8,6 +8,10 @@ import VCardTextarea from './VCardTextarea/VCardTextarea'
 
 const App = () => (
   <Container>
+    <Group align="baseline">
+      <Title>Miani</Title>
+      <Text>{__APP_VERSION__}</Text>
+    </Group>
     <Group align="start">
       <Stack classNames={{ root: classes['FormStack'] }}>
         <VCardForm />
--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,58 @@
 import { exec as execSync } from 'node:child_process'
 import { promisify } from 'node:util'
 import preact from '@preact/preset-vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import topLevelAwait from 'vite-plugin-top-level-await'
 
 const exec = promisify(execSync)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    preact({
-      babel: {
-        plugins: [
-          [
-            'formatjs',
-            {
-              idInterpolationPattern: '[sha512:contenthash:base64:6]',
-              ast: true,
-            },
-          ],
-        ],
-      },
-    }),
-    {
-      name: 'Extract Messages',
-      async closeBundle() {
-        await exec(
-          "bun x formatjs extract 'src/**/*.tsx' --out-file dev-dist/lang.json",
-        )
-        await exec(
-          'bun x formatjs compile dev-dist/lang.json --out-file src/messages/en.json',
-        )
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      __APP_VERSION__: JSON.stringify(env.npm_package_version),
     },
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'miani',
+    plugins: [
+      preact({
+        babel: {
+          plugins: [
+            [
+              'formatjs',
+              {
+                idInterpolationPattern: '[sha512:contenthash:base64:6]',
+                ast: true,
+              },
+            ],
+          ],
+        },
+      }),
+      {
+        name: 'Extract Messages',
+        async closeBundle() {
+          await exec(
+            "bun x formatjs extract 'src/**/*.tsx' --out-file dev-dist/lang.json",
+          )
+          await exec(
+            'bun x formatjs compile dev-dist/lang.json --out-file src/messages/en.json',
+          )
+        },
       },
-      devOptions: {
-        enabled: true,
-      },
-    }),
-    topLevelAwait({
-      promiseExportName: '__tla',
-      promiseImportName: (i) => `__tla_${i}`,
-    }),
-  ],
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'miani',
+        },
+        devOptions: {
+          enabled: true,
+        },
+      }),
+      topLevelAwait({
+        promiseExportName: '__tla',
+        promiseImportName: (i) => `__tla_${i}`,
+      }),
+    ],
+  }
 })
